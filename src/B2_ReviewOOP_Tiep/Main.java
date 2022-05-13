ccbbca8377962782d96d6b96c459693e09d956be
HangNT169 <hN160499aA@>
2022-05-13 10:24:18 +0700

    Buoi 2 Review OOP_Tiep

---
+++ src/B2_ReviewOOP_Tiep/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package B2_ReviewOOP_Tiep;
+
+/**
+ *
+ * @author hangnt
+ */
+public class Main {
+    public static void main(String[] args) {
+//        Nguoi nguoi = new Nguoi();
+//        nguoi.display();
+//        Nguoi nguoi2 = new SinhVien();
+//        nguoi2.display();
+    }
+}
