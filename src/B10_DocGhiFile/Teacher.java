f9d39dcd4cf031061b461da061ddc1a855a73885
HangNT169 <hN160499aA@>
2022-05-30 11:11:37 +0700

    Update

--- src/B10_DocGhiFile/Teacher.java
+++ src/B10_DocGhiFile/Teacher.java
@@ -16,7 +16,6 @@ public class Teacher implements Serializable { // Cho tat ca thuoc tinh khi ghi 
     private int tuoi;
     private String namSinh;
     private String diaChi;
-    private static final long serialVersionUID = 1L;
 
     public Teacher() {
     }
